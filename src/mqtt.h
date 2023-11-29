c80420bb8e36671f4fa46a8a958ab61dd97ad0a9
NachoEstevo <ignacio.estevo@ing.austral.edu.ar>
2023-11-29 05:37:44 -0300

    fixes

--- src/mqtt.h
+++ src/mqtt.h
@@ -1,13 +1,3 @@
-
-/*
- *  Project 41-mqtt_00 - Austral - EAM
- *
- *  mqtt.h
- *      Manage conversation with MQTT broker
- */
-
 void init_mqtt(int num);                                //  All initializations needed for conversation with broker
 void do_publish(const char *topic, const char *msg);    //  Called to publish in broker
 void test_mqtt(void);                                   //  Update to see broker news
-
-
